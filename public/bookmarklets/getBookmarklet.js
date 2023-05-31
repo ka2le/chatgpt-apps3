6d5366d44f241eefce546faae51ddf85640b21a6
kalle.kindvall <kalle.kindvall@dacsa.se>
2023-05-31 21:28:23 +0200

    fallback

--- public/bookmarklets/getBookmarklet.js
+++ public/bookmarklets/getBookmarklet.js
@@ -1,18 +1,30 @@
 javascript:(function(){
-    function reloadScript() {
-        console.log("Reloading");
-        var oldScript = document.getElementById('myBookmarklet');
-        var newScript = document.createElement('script');
-        newScript.id = 'myBookmarklet';
-        newScript.src = 'http://localhost:3000/chatgpt-apps3/bookmarklets/gptEnhanceMain.js?' + new Date().getTime();
-        oldScript.parentNode.replaceChild(newScript, oldScript);
-    }
-
+    var head = document.getElementsByTagName('head')[0];
     var element = document.createElement('script');
-    element.id = 'myBookmarklet';
-    element.src = 'http://localhost:3000/chatgpt-apps3/bookmarklets/gptEnhanceMain.js?' + new Date().getTime();
-    document.body.appendChild(element);
+    element.id = 'myBookmarkletScript';
+    var mainUrl = 'http://localhost:3000/chatgpt-apps3/bookmarklets/gptEnhanceMain.js?' + new Date().getTime();
+    var fallbackUrl = 'https://ka2le.github.io/chatgpt-apps3/bookmarklets/gptEnhanceMain.js?' + new Date().getTime();
 
-    var reloadInterval = 60000;  
-    setInterval(reloadScript, reloadInterval);
+    element.src = mainUrl;
+    element.onload = function() {
+        console.log('Main script loaded successfully');
+    };
+    element.onerror = function() {
+        console.log('Failed to load main script, trying fallback');
+        loadFallback();
+    };
+    head.appendChild(element);
+
+    function loadFallback() {
+        var fallbackElement = document.createElement('script');
+        fallbackElement.id = 'myBookmarkletFallbackScript';
+        fallbackElement.src = fallbackUrl;
+        fallbackElement.onload = function() {
+            console.log('Fallback script loaded successfully');
+        };
+        fallbackElement.onerror = function() {
+            console.log('Failed to load fallback script');
+        };
+        head.appendChild(fallbackElement);
+    }
 })();
